f35e75b6b13a1e8ed6b9bfa32a6f7f2f7d937c9b
Juan Antonio Martin <juanantoniomartinb@gmail.com>
2020-06-29 13:04:38 +0200

    User model, types and resolvers created

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,9 +1,35 @@
-
+const Usuario = require('../models/Usuario')
+const bcryptjs = require('bcryptjs')
 
 // Resolvers
 const resolvers = {
     Query: {
         obtenerCurso: () => "Algo"
+    },
+    Mutation: {
+        nuevoUsuario: async (_, { input }) => {
+            // Revisar si el usuario ya está registrado
+            const { email, password } = input
+            const existeUsuario = await Usuario.findOne({ email })
+
+            if (existeUsuario) {
+                throw new Error('El usuario ya está registrado')
+            }
+            // Hashear el password
+            const salt = await bcryptjs.genSalt(10)
+            input.password = await bcryptjs.hash(password, salt)
+
+
+            try {
+                // Guardarlo en BD
+                const usuario = new Usuario(input)
+                usuario.save()
+                return usuario
+
+            } catch (error) {
+
+            }
+        }
     }
 }
 
